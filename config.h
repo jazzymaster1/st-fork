e1f815299b6b6aaf0a66e90f3915ff6d698ac97f
jazzymaster1 <141975423+jazzymaster1@users.noreply.github.com>
2023-10-03 02:01:06 -0300

    Update config.h

--- config.h
+++ config.h
@@ -1,12 +1,6 @@
-/* See LICENSE file for copyright and license details. */
-
 /*
- * appearance
- *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-
-
 
 static char *font = "Ubuntu Mono:pixelsize=19:antialias=true:autohint=true";
 /* static char *font = "Liberation Mono:pixelsize=19:antialias=true:autohint=true"; */
@@ -96,11 +90,6 @@ char *termname = "st-256color";
  *	stty tabs
  */
 unsigned int tabspaces = 8;
-
-
-
-
-
 
 
 
@@ -451,122 +440,9 @@ static char ascii_printable[] =
 
 
 
-
+// tudo relacionado a coloracao por favor colocar neste arquivo c        agradecido
 
 #include "temas.c"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* 
- *
- * C O R E S
- *
- *
- * */
-
-
-/////////// PADRAO
-
-/* /1* Terminal colors (16 first used in escape sequence) *1/ */
-/* static const char *colorname[] = { */
-/* 	/1* 8 normal colors *1/ */
-/* 	"black", */
-/* 	"red3", */
-/* 	"green3", */
-/* 	"yellow3", */
-/* 	"blue2", */
-/* 	"magenta3", */
-/* 	"cyan3", */
-/* 	"gray90", */
-
-/* 	/1* 8 bright colors *1/ */
-/* 	"gray50", */
-/* 	"red", */
-/* 	"green", */
-/* 	"yellow", */
-/* 	"#5c5cff", */
-/* 	"magenta", */
-/* 	"cyan", */
-/* 	"white", */
-
-/* 	[255] = 0, */
-
-/* 	/1* more colors can be added after 255 to use with DefaultXX *1/ */
-/* 	"#cccccc", */
-/* 	"#555555", */
-/* 	"gray90", /1* default foreground colour *1/ */
-/* 	"black", /1* default background colour *1/ */
-/* }; */
-
-/*/1* */
-/* * Default colors (colorname index) */
-/* * foreground, background, cursor, reverse cursor */
-/* *1/ */
-/*unsigned int defaultfg = 258; */
-/*unsigned int defaultbg = 259; */
-/*unsigned int defaultcs = 256; */
-/*static unsigned int defaultrcs = 257; */
-
-
-
-/*/1* 	google-dark		 *1/ */
-/*/1* Terminal colors (16 first used in escape sequence) *1/ */
-/*static const char *colorname[] = { */
-
-/*  /1* 8 normal colors *1/ */
-/*  [0] = "#1d1f21", /1* black   *1/ */
-/*  [1] = "#cc342b", /1* red     *1/ */
-/*  [2] = "#198844", /1* green   *1/ */
-/*  [3] = "#fba922", /1* yellow  *1/ */
-/*  [4] = "#3971ed", /1* blue    *1/ */
-/*  [5] = "#a36ac7", /1* magenta *1/ */
-/*  [6] = "#3971ed", /1* cyan    *1/ */
-/*  [7] = "#c5c8c6", /1* white   *1/ */
-
-/*  /1* 8 bright colors *1/ */
-/*  [8]  = "#969896", /1* black   *1/ */
-/*  [9]  = "#cc342b", /1* red     *1/ */
-/*  [10] = "#198844", /1* green   *1/ */
-/*  [11] = "#fba922", /1* yellow  *1/ */
-/*  [12] = "#3971ed", /1* blue    *1/ */
-/*  [13] = "#a36ac7", /1* magenta *1/ */
-/*  [14] = "#3971ed", /1* cyan    *1/ */
-/*  [15] = "#ffffff", /1* white   *1/ */
-
-/*  /1* special colors *1/ */
-/*  [256] = "#1d1f21", /1* background *1/ */
-/*  [257] = "#c5c8c6", /1* foreground *1/ */
-/*}; */
-
-/*/1* */
-/* * Default colors (colorname index) */
-/* * foreground, background, cursor */
-/* *1/ */
-/*unsigned int defaultfg = 257; */
-/*unsigned int defaultbg = 256; */
-/*unsigned int defaultcs = 257; */
-
-/*/1* */
-/* * Colors used, when the specific fg == defaultfg. So in reverse mode this */
-/* * will reverse too. Another logic would only make the simple feature too */
-/* * complex. */
-/* *1/ */
-/*static unsigned int defaultitalic = 7; */
-/*static unsigned int defaultunderline = 7; */
-
-/*static unsigned int defaultrcs = 257; */
-
 
 
 
